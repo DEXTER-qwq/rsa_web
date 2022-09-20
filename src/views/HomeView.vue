333a17b67b1b43f888c99098204a447a496eb754
DEXTER-qwq <3165862400@qq.com>
2022-09-20 19:50:04 +0800

    除核心功能均解决

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <My_index></My_index>
+</template>
+
+<script>
+// @ is an alias to /src
+import My_index from '../components/My_index.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    My_index
+  }
+}
+</script>
